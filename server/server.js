3c82cb9f9cba8b77316b8464701d844c43aced33
최은철 <wndqhrgkdlel@gmail.com>
2024-02-20 11:36:40 +0900

    길드와 비슷한 세팅

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path')
+
+// 정적 파일을 제공하기 위한 폴더 설정
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+// 기본 경로로 접근했을 때 응답
+app.get('/', (req, res) => {
+  res.send('Express 서버가 실행중입니다!');
+});
+
+// 서버 시작
+app.listen(port, () => {
+  console.log(`서버가 http://localhost:${port} 에서 실행중입니다!`);
+});
